0755228a04f1dedab17496ccd646de2ea8d4cb8f
Lorenzo Mazzarotto <mazzaxc@icloud.com>
2024-11-11 07:43:55 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Lorenzo Mazzarotto on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
